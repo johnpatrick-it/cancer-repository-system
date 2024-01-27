42f5428335dd68296d53f608fe9ecf0d8c04f94f
johnpatrick-it <johnpatrickhaguimit@gmail.com>
2024-01-27 23:41:53 +0800

    Fixing bug and Adding hospital_id
    Fix hospital_id FK for patient_form

--- patientform/patient-registry-one-submit.php
+++ patientform/patient-registry-one-submit.php
@@ -21,60 +21,73 @@ if (!$db_connection) {
     die("Error connecting to the database: " . pg_last_error());
 }
 
+
+$query_fetch_hospital_id = "SELECT hospital_id FROM public.repo_user WHERE repo_user_id = $1";
+$params_fetch_hospital_id = array($_SESSION['repo_user_id']);
+$result_hospital_id = pg_query_params($db_connection, $query_fetch_hospital_id, $params_fetch_hospital_id);
+
+if ($result_hospital_id) {
+    $row_hospital_id = pg_fetch_assoc($result_hospital_id);
+    $hospital_id = $row_hospital_id['hospital_id'];
+
     // Retrieving data sa patient-registry-one.php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $type_of_patient = $_POST['type_of_patient'];
-    $patient_last_name = $_POST['patient_lname_initial'];
-    $patient_first_name = $_POST['patient_fname_initial'];
-    $patient_middle_name = $_POST['patient_mname'];
-    $sex = $_POST['sex'];
-    $civil_status = $_POST['civil_status'];
-    $birthday = $_POST['birthday'];
-    $address_region = $_POST['address_region'];
-    $address_barangay = $_POST['address_barangay'];
-    $address_province = $_POST['address_province'];
-    $address_city_municipality = $_POST['address_city_municipality'];
-    $nationality = $_POST['nationality'];
-    $occupation = $_POST['occupation'];
-    $educational_attainment = $_POST['educational_attainment'];
-    $race = $_POST['race'];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $type_of_patient = $_POST['type_of_patient'];
+        $patient_last_name = $_POST['patient_lname_initial'];
+        $patient_first_name = $_POST['patient_fname_initial'];
+        $patient_middle_name = $_POST['patient_mname'];
+        $sex = $_POST['sex'];
+        $civil_status = $_POST['civil_status'];
+        $birthday = $_POST['birthday'];
+        $address_region = $_POST['address_region'];
+        $address_barangay = $_POST['address_barangay'];
+        $address_province = $_POST['address_province'];
+        $address_city_municipality = $_POST['address_city_municipality'];
+        $nationality = $_POST['nationality'];
+        $occupation = $_POST['occupation'];
+        $educational_attainment = $_POST['educational_attainment'];
+        $race = $_POST['race'];
 
-    $repoUserId = $_SESSION['repo_user_id'];
+        $repoUserId = $_SESSION['repo_user_id'];
+    
 
-    // Insert SQL query to public.patient_general_info 
+    // Insert SQL query to public.patient_general_info
     $query = "INSERT INTO public.patient_general_info (
         type_of_patient, patient_last_name, patient_first_name, patient_middle_name,
-        sex, civil_status, birthday, nationality,
-        occupation, educational_attainment, race, repo_user_id, address_region, address_barangay, address_province, address_city_municipality
+        sex, civil_status, birthday, nationality, occupation, educational_attainment, 
+        race, repo_user_id, hospital_id, address_region, address_barangay, address_province, address_city_municipality
     ) 
     VALUES (
         $1, $2, $3, $4,
         $5, $6, $7, $8, $9,
-        $10, $11, $12, $13, $14, $15, $16
-
+        $10, $11, $12, $13, 
+        $14, $15, $16, $17
     ) RETURNING patient_id";
-//RETURNING patient_id (VERY IMPORTANT FOR FK IN OTHER FORMS)
+
     $params = array(
         $type_of_patient, $patient_last_name, $patient_first_name, $patient_middle_name,
         $sex, $civil_status, $birthday, $nationality,
-        $occupation, $educational_attainment, $race, $repoUserId,
+        $occupation, $educational_attainment, $race, $repoUserId, $hospital_id,
         $address_region, $address_barangay, $address_province, $address_city_municipality
     );
 
     $result_patient_id = pg_query_params($db_connection, $query, $params);
-//FUNCTION OF THIS CODE IS TO CHECK KUNG MERON NA DAGDAG NA ROW PAG KA SUBMIT 
-if ($result_patient_id) {
-    $row_patient_id = pg_fetch_assoc($result_patient_id);
-    $patient_id = $row_patient_id['patient_id'];
 
-    //Storing patient_id session (VERY IMPORTANT!)
-    $_SESSION['patient_id'] = $patient_id;
+    if ($result_patient_id) {
+        $row_patient_id = pg_fetch_assoc($result_patient_id);
+        $patient_id = $row_patient_id['patient_id'];
 
-    // Redirect to patient-registry-two.php
-    header('Location: patient-registry-two.php');
-    exit;
-} 
+        // Storing patient_id session (VERY IMPORTANT!)
+        $_SESSION['patient_id'] = $patient_id;
 
-
+        // Redirect to patient-registry-two.php
+        header('Location: patient-registry-two.php');
+        exit;
+    } else {
+        // Handle the error case for the patient insertion
+        die("Error inserting patient information: " . pg_last_error());
+    }
+} }else {
+    // Handle the error case for fetching hospital_id
+    die("Error fetching hospital_id: " . pg_last_error());
 }
-pg_close($db_connection);
