315d1d4f7ef4ef1ba5d9fa2a339428137c252147
Francis Gagatiga <francis.gorrion.gagatiga@gmail.com>
2024-02-05 23:16:25 +0800

    my commit in develop

--- includes/user-sidebar.php
+++ includes/user-sidebar.php
@@ -1,39 +1,39 @@
 <style>
-    @media (max-width: 768px) {
-        .sidebar {
-            display: none;
-        }
+@media (max-width: 768px) {
+    .sidebar {
+        display: none;
     }
+}
 
-    /* User Profile Img */
-    .neon-border {
-        border: 2px solid #0B72BD;
-        box-shadow: 0 0 10px #0B72BD;
-    }
+/* User Profile Img */
+.neon-border {
+    border: 2px solid #0B72BD;
+    box-shadow: 0 0 10px #0B72BD;
+}
 
-    .user-img img {
-        width: 6rem;
-        height: auto;
-    }
+.user-img img {
+    width: 6rem;
+    height: auto;
+}
 
-    .profile-block {
-        margin: 0 0 0 -2rem;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+.profile-block {
+    margin: 0 0 0 -2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
 
-    .user-role {
-        margin-top: -1.5rem;
-    }
+.user-role {
+    margin-top: -1.5rem;
+}
 
 
-    /* LOGOUT */
-    .out-container .out-button {
-        position: fixed;
-        bottom: 0;
-        left: 0;
-    }
+/* LOGOUT */
+.out-container .out-button {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+}
 </style>
 
 <div class="sidebar" id="sidebar">
@@ -43,22 +43,26 @@
                 <li class="profile-block">
                     <a href="#">
                         <span class="user-img d-inline-block position-relative">
-                            <img src="./profiles/pcc-logo.png" alt="User Picture" class="rounded-circle img-thumbnail neon-border">
+                            <img src="../profiles/pcc-logo.png" alt="User Picture"
+                                class="rounded-circle img-thumbnail neon-border">
                         </span>
                     </a>
                     <br>
-                    <a href="../admin-index.php"><span class="text-white small user-role">CANCER REPOSITORY ADMIN</span></a>
+                    <a href="../admin-index.php"><span class="text-white small user-role">CANCER REPOSITORY
+                            ADMIN</span></a>
                 </li>
                 <br>
                 <br>
                 <br>
 
                 <!-- User Dashboard -->
-                <li><a href="./user-landing-page.php"><i class="la la-dashboard"></i><span>User Dashboard</span></a></li>
+                <li><a href="./user-landing-page.php"><i class="la la-dashboard"></i><span>User Dashboard</span></a>
+                </li>
 
                 <!-- Patient Registry -->
-                <li><a href="./patientform/patient-registry-one.php"><i class="la la-user-plus"></i><span>Patient Registry</span></a></li>
-                
+                <li><a href="./patientform/patient-form.php"><i class="la la-user-plus"></i><span>Patient
+                            Registry</span></a></li>
+
                 <!-- Manage Patient -->
                 <li><a href="./manage-patient.php"><i class="la la-users"></i><span>Manage Patient</span></a></li>
 
